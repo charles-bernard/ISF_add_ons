e2143ab0bb8db1f66558ce0a202e1dab0cfdbe68
Charles <charles@7138-50.snv.jussieu.fr>
2019-01-21 16:56:01 +0100

    read tree

--- 3_ISF_to_diversity_visualisation/visualize_diversity.py
+++ 3_ISF_to_diversity_visualisation/visualize_diversity.py
@@ -8,6 +8,23 @@ from ete3 import *
 
 ncbi = NCBITaxa()
 
+
+def assign_lineage_to_leaf(node):
+    if node.is_leaf():
+        if node.name != 'NA':
+            try:
+                node_lineage = ncbi.get_lineage(int(node.name))
+            except:
+                node_lineage = ['NA']
+        else:
+            node_lineage = ['NA']
+        node.lineage = node_lineage
+    else:
+        node_children = node.children
+        for i in range(0, len(node_children)):
+            assign_lineage_to_node(node_children[i])
+
+
 parser = argparse.ArgumentParser(
     description='This script runs rpsblast with the ISF output gene family in fasta '
                 'against COG db in order to assign functions to all the representative '
@@ -15,8 +32,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-i', '--input_dir', dest='input_dir', type=str,
                     help='specify that path to a directory that must store the '
                          'Multitwin_edges files of gene families (*_Multitwin_edges.csv)')
-parser.add_argument('-t', '--reference_tree', dest='reference_tree', type=str,
-                    help='specify that path to a reference phylogenetic tree in Newick format')
+parser.add_argument('-t', '--reference_tree', dest='tree', type=str,
+                    help='specify that path to a reference phylogenetic tree in Newick format'
+                         '(the name of each leaf must correspond to a NCBI taxonomic id)')
 args = parser.parse_args()
 
 # LOAD EDGES FILES
@@ -24,11 +42,13 @@ list_edges_files = glob(os.path.join(args.input_dir, '*MultiTwin_edges.csv'))
 list_family_names = list()
 dict_species = dict()
 dict_taxons = dict()
+dict_lineage = dict()
 
 for i in range(0, len(list_edges_files)):
     list_family_names.append(os.path.basename(list_edges_files[i]).split("_MultiTwin_edges.csv")[0])
     list_species = list()
     list_taxons = list()
+    list_lineages = list()
     with open(list_edges_files[i]) as f:
         next(f)
         for row in f:
@@ -37,13 +57,16 @@ for i in range(0, len(list_edges_files)):
                 int(curr_species)
                 list_taxons.append(curr_species)
                 list_species.appendlist(ncbi.get_taxid_translator([curr_species]).values())[0]
+                list_lineages.append(ncbi.get_lineage(list_taxons[-1]))
             except:
                 list_species.append(curr_species)
                 translated_name = list(ncbi.get_name_translator([curr_species]).values())
                 if translated_name:
                     list_taxons.append(translated_name[0][0])
+                    list_lineages.append(ncbi.get_lineage(list_taxons[-1]))
                 else:
-                    list_taxons.append('')
+                    list_taxons.append('NA')
+                    list_lineages.append('[NA]')
     f.close()
     dict_species[list_family_names[i]] = list_species
     dict_taxons[list_family_names[i]] = list_taxons
@@ -51,5 +74,8 @@ for i in range(0, len(list_edges_files)):
     print(dict_taxons['CDPS'])
 
 # LOAD REFERENCE TREE
+tree = Tree(args.tree, format=1)
+assign_lineage_to_leaf(tree)
+
 
 
